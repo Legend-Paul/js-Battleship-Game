59cff2b4dc6b8e786a63b1c5e16a155d18de7cf2
Legend Paul <qamunyamaina950@gmail.com>
2025-05-06 22:06:18 +0300

    Implimet js input validation

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,17 @@
 let dotsCont = document.querySelector(".dots-cont");
 let dialog = document.querySelector(".dialog");
+let startGameBtn = document.querySelector(".start-game-btn");
+let gamePlayOption = document.querySelector(".game-play-option");
+let firstPlayer = document.querySelector(".first-player-name");
+let secondPlayerLabel = document.querySelector(".second-player-label");
+let secondPlayer = document.querySelector(".second-player-name");
+let playersName = document.querySelectorAll(".players-name");
+let errorMsg = document.querySelector(".error-msg");
+
 let counter = 0;
+let playOption = null;
+let players = {};
+let twoPlayers = false;
 
 function displayCurrentDot(time) {
     return new Promise((resolve) => {
@@ -59,5 +70,64 @@ let showDialog = () => {
     dialog.showModal();
 };
 setTimeout(showDialog, 6000);
+
+function getPlayers() {
+    gamePlayOption.addEventListener("change", (e) => {
+        let firstPlayerTitle = document.querySelector(".first-player-label p");
+        playOption = gamePlayOption.value;
+        errorMsg.innerHTML = "";
+        if (playOption === "2 players") {
+            secondPlayerLabel.style.display = "grid";
+            firstPlayerTitle.innerHTML = "First player name:";
+            twoPlayers = true;
+        } else {
+            secondPlayerLabel.style.display = "none";
+            firstPlayerTitle.innerHTML = "Player name:";
+            twoPlayers = false;
+        }
+    });
+}
+getPlayers();
+function startGame() {
+    startGameBtn.addEventListener("click", (e) => {
+        if (!firstPlayer.value || (twoPlayers && !secondPlayer.value)) {
+            if (playOption === "2 players") {
+                errorMsg.innerHTML = "Provide all players name";
+            } else if (playOption === "AI") {
+                errorMsg.innerHTML = "Provide player name";
+            } else {
+                errorMsg.innerHTML =
+                    "Select play option or provide player name";
+            }
+        } else {
+            addPlayers(firstPlayer, secondPlayer);
+        }
+    });
+}
+function addPlayers(firstPlayer, secondPlayer) {
+    let player1 = firstPlayer.value.toLowerCase();
+    let player2 = null;
+    if (playOption == "2 players") {
+        player2 = secondPlayer.value;
+    } else {
+        player2 = "AI";
+    }
+
+    if (player1.toLowerCase() !== player2.toLowerCase()) {
+        players.player1 = player1;
+        players.player2 = player2;
+        return players;
+    }
+    errorMsg.innerHTML = "Enter different names";
+}
+function removeRedOutline() {
+    playersName.forEach((player) => {
+        player.addEventListener("focus", () => {
+            errorMsg.innerHTML = "";
+        });
+    });
+}
+removeRedOutline();
+startGame();
 
 export { loadDots };
