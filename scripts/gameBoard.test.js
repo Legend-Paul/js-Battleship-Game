d4c3f02fc664a4c86d9ce90cf7bc6772f50b0459
Legend Paul <qamunyamaina950@gmail.com>
2025-05-06 12:52:16 +0300

    Add tests for Gameboard ship placement limits

---
+++ scripts/gameBoard.test.js
@@ -0,0 +1,110 @@
+const { Gameboard } = require("./gameboard.js");
+
+describe("left limit", () => {
+    test("ship over grid left limit", () => {
+        let shipPos = { x: 10, y: 15, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            false
+        );
+    });
+    test("ship within grid left limit", () => {
+        let shipPos = { x: 20, y: 15, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            true
+        );
+    });
+});
+
+describe("top limit", () => {
+    test("ship above grid top limit", () => {
+        let shipPos = { x: 20, y: 10, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            false
+        );
+    });
+    test("ship within grid top limit", () => {
+        let shipPos = { x: 20, y: 150, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            true
+        );
+    });
+});
+
+describe("right limit", () => {
+    test("Horizontal ship over grid right limit", () => {
+        let shipPos = { x: 250, y: 150, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            false
+        );
+    });
+    test("Horizontal ship within grid right limit", () => {
+        let shipPos = { x: 20, y: 15, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            true
+        );
+    });
+    test("Vertical ship over grid right limit", () => {
+        let shipPos = { x: 390, y: 10, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+
+        expect(gameboard.placeShip(shipPos, 5, "vertical", boardPos)).toEqual(
+            false
+        );
+    });
+    test("Vertical ship within grid right limit", () => {
+        let shipPos = { x: 250, y: 20, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "vertical", boardPos)).toBe(
+            true
+        );
+    });
+});
+
+describe("bottom limit", () => {
+    test("Horizontal ship below grid bottom limit", () => {
+        let shipPos = { x: 30, y: 390, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            false
+        );
+    });
+    test("Horizontal ship within grid bottom limit", () => {
+        let shipPos = { x: 20, y: 15, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "horizontal", boardPos)).toBe(
+            true
+        );
+    });
+    test("Vertical ship below grid bottom limit", () => {
+        let shipPos = { x: 20, y: 250, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "vertical", boardPos)).toBe(
+            false
+        );
+    });
+    test("Vertical ship within grid bottom limit", () => {
+        let shipPos = { x: 20, y: 15, width: 40, height: 40 };
+        let boardPos = { x: 15, y: 15, width: 400, height: 400 };
+        let gameboard = new Gameboard();
+        expect(gameboard.placeShip(shipPos, 5, "vertical", boardPos)).toBe(
+            true
+        );
+    });
+});
